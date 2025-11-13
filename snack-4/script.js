e0a40fbb3859df9fbab377a5a9e516f358cdedd0
Antimo-M <montellaantimo@live.it>
2025-11-13 15:42:33 +0100

    feat: fatto fino esercizio 6

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const autori = posts.map(function(autore){
+  return autore.author
+})
+
+console.log(autori)
--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,12 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+const zucchineGrandi = zucchine.filter(function(verdura){
+  return  verdura.length >= 15;
+})
+console.log(zucchineGrandi)
+
+const zucchinePiccole = zucchine.filter(function(piccole){
+   return piccole.length < 15;
+})
+console.log(zucchinePiccole)
